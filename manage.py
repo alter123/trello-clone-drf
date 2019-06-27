8c42f993a0999c5137af7f41acdf67a4ebf3cc84
alter123 <jay.v@lendenapp.com>
2019-06-27 23:32:12 +0530

    Switch to default settings.

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     os.environ.setdefault(
                 'DJANGO_SETTINGS_MODULE', 
-                'trelloclone.local_settings'
+                'trelloclone.settings'
             )
     try:
         from django.core.management import execute_from_command_line
